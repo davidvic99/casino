022a25c4da262e4d073ee4829f9237321375ce0b
ALUMNADO\davima <davima@floridauniverstaria.es>
2018-09-26 12:58:45 +0200

    version2.1

--- index.php
+++ index.php
@@ -10,24 +10,24 @@
 
 
 var dado = Math.floor((Math.random() * 6) + 1);
-      var cartera = prompt("Añade saldo a tu cartera");
+      var cartera = parseInt(prompt("Añade saldo a tu cartera"));
       alert("Normas del juego, si sale 4,5,6 gana el casino, si sale 1,2,3 ganas tú. La apuesta del casino es la misma que la tuya. No puedes apostar mas de 50€");
-      var dinero = prompt("Introduce tu dinero de juego ");
+      var dinero = parseInt(prompt("Introduce tu dinero de juego "));
       var dincasino =  dinero;
       var dineroactual = 50;
-      if(dineroactual < dinero) {
-        alert("No puedes usar esa cantidad porque supera nuestro limite")
+      if(cartera < dinero) {
+        alert("No puedes usar esa cantidad porque supera tu limite")
       }
       else {
         alert("Tu apuesta es" + " " + dinero + "€" );
          if (dado > 3) {
            (dinero2 = (dinero - dincasino)) ;
-            cartera1 = (cartera - (dinero2 - -dincasino));
+            cartera1 = (cartera - (dinero2 + dincasino));
 
 
          } else if (dado < 4) {
-           (dinero2 = (dinero - -dincasino)) ;
-            cartera1 =  (cartera - -(dinero2 - dincasino));
+           (dinero2 = (dinero  + dincasino)) ;
+            cartera1 =  (cartera  + (dinero2 - dincasino));
 
 
          };
@@ -35,7 +35,7 @@ var dado = Math.floor((Math.random() * 6) + 1);
          alert("Salio" + " " + dado);
 
          alert("Tu dinero actual es " + dinero2 + "€" );
-          alert("tu dinero en la cartera es" + cartera1);
+          alert("tu dinero en la cartera es de " + cartera1 + "€");
              var dinero3=dinero2;
 
       }
