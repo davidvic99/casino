3baf2aba793b1c97eb95c2acea6477514d00abad
ALUMNADO\davima <davima@floridauniverstaria.es>
2018-09-26 13:15:29 +0200

    ordenado la cosa esta

--- index.php
+++ index.php
@@ -10,35 +10,44 @@
 
 
 var dado = Math.floor((Math.random() * 6) + 1);
-      var cartera = parseInt(prompt("Añade saldo a tu cartera"));
-      alert("Normas del juego, si sale 4,5,6 gana el casino, si sale 1,2,3 ganas tú. La apuesta del casino es la misma que la tuya. No puedes apostar mas de 50€");
-      var dinero = parseInt(prompt("Introduce tu dinero de juego "));
-      var dincasino =  dinero;
-      var dineroactual = 50;
-      if(cartera < dinero) {
+var cartera = parseInt(prompt("Añade saldo a tu cartera"));
+alert("Normas del juego, si sale 4,5,6 gana el casino, si sale 1,2,3 ganas tú. La apuesta del casino es la misma que la tuya. No puedes apostar mas de 50€");
+var dinero = parseInt(prompt("Introduce tu dinero de juego "));
+var dincasino =  dinero;
+var dineroactual = 50;
+
+
+if(cartera < dinero) {
+
         alert("No puedes usar esa cantidad porque supera tu limite")
       }
-      else {
+
+else {
         alert("Tu apuesta es" + " " + dinero + "€" );
-         if (dado > 3) {
-           (dinero2 = (dinero - dincasino)) ;
-            cartera1 = (cartera - (dinero2 + dincasino));
 
+       if (dado > 3) {
 
-         } else if (dado < 4) {
+         (dinero2 = (dinero - dincasino)) ;
+
+          cartera1 = (cartera - (dinero2 + dincasino));
+
+    } else if (dado < 4) {
+
            (dinero2 = (dinero  + dincasino)) ;
+
             cartera1 =  (cartera  + (dinero2 - dincasino));
 
 
          };
 
-         alert("Salio" + " " + dado);
+      alert("Salio" + " " + dado);
+      alert("Tu dinero actual es " + dinero2 + "€" );
+      alert("tu dinero en la cartera es de " + cartera1 + "€");
 
-         alert("Tu dinero actual es " + dinero2 + "€" );
-          alert("tu dinero en la cartera es de " + cartera1 + "€");
-             var dinero3=dinero2;
+      var dinero3=dinero2;
 
       }
+
       //Ronda dos
              var si = prompt("Deseas repetir la jugada. Por favor utiliza si o no");
              if (si = si) {
