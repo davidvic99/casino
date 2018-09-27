5c73bab74c0c41ef638d6ff7b8c9939e8538654a
ALUMNADO\davima <davima@floridauniverstaria.es>
2018-09-27 10:49:22 +0200

    Version 3 Con 1 rondas

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
 
   <script type="text/javascript">
 
-
+//Cartera + dinero + juego
 var dado = Math.floor((Math.random() * 6) + 1);
 var cartera = parseInt(prompt("Añade saldo a tu cartera"));
 alert("Normas del juego, si sale 4,5,6 gana el casino, si sale 1,2,3 ganas tú. La apuesta del casino es la misma que la tuya. No puedes apostar mas de 50€");
@@ -40,37 +40,64 @@ else {
 
          };
 
-      alert("Salio" + " " + dado);
-      alert("Tu dinero actual es " + dinero2 + "€" );
-      alert("tu dinero en la cartera es de " + cartera1 + "€");
+  alert("Salio" + " " + dado);
+  alert("Tu dinero actual es " + dinero2 + "€" );
+  alert("Tu dinero en la cartera es de " + cartera1 + "€");
 
-      var dinero3=dinero2;
+  var dinero3=dinero2;
 
       }
 
-      //Ronda dos
-             var si = prompt("Deseas repetir la jugada. Por favor utiliza si o no");
-             if (si = si) {
-               var dado2 = Math.floor((Math.random() * 6) + 1);
-               var dincasino2 = dinero3;
+  //Rondas
 
 
-               if (dado2>3) {
-              (dinero3 = (dinero2 - dincasino2)) ;
+  var ronda = parseInt(prompt("¿Cuantas veces deseas repetir el juego?"));
+  if (ronda<=0) {
+  alert("Gracias por jugar.");
 
-            } else if (dado2<4) {
-              (dinero3 = (dinero2 - -dincasino2)) ;
+      } else if (ronda>=ronda) {
+        var dado2 = Math.floor((Math.random() * 6) + 1);
+  alert("tu dinero actual de la cartera es " + cartera1);
+      if (cartera1<0) {
+          alert("No tienes dinero suficente. Gracias por jugar")
 
-            }
+      }
+  var dinero4 = parseInt(prompt("¿Cuánto dinero quieres apostar de la cartera?"));
+  var dincasino2 =  dinero4;
+  if (dinero4>cartera1) {
+    alert("No dispones de tanto dinero en la cartera");
 
 
-            alert("Salio" + " " + dado);
+  } else if (dado2>3) {
+    dinero5 = (dinero4 - dincasino2) ;
+
+     cartera2 = (cartera1 - (dinero4 + dincasino2));
 
 
-            alert("Tu dinero actual es " + dinero3 + " €");
+  } else if (dado2<4) {
+    (dinero5 = (dinero4  + dincasino2)) ;
 
-           }
+     cartera2 =  (cartera1  + (dinero4 - dincasino2));
 
+  }
+
+if (cartera2===0) {
+  alert(dado2)
+  alert("tu dinero es 0. Se acabo.")
+} else {
+  alert(dado2)
+  var cartera2=(cartera1 + dinero5);
+  alert("Tu dinero actual es " + dinero5 + "€" );
+  alert("Tu dinero en la cartera es de " + cartera2 + "€");
+}
+
+
+
+
+
+
+
+      }
 
      </script>
 
